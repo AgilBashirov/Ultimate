137a3ef667ae9420c83f124c1f3987937371917d
agil.bashirov <agil.bashirov@azintelecom.az>
2023-05-19 17:52:50 +0400

    100

--- 02-Configuring a Logging Service in .NET Core/CompanyEmployees/MappingProfile.cs
+++ 02-Configuring a Logging Service in .NET Core/CompanyEmployees/MappingProfile.cs
@@ -8,13 +8,15 @@ namespace CompanyEmployees
     {
         public MappingProfile()
         {
-            //CreateMap<Company, CompanyDto>()
-            //.ForMember(c => c.FullAddress, opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
-
             CreateMap<Company, CompanyDto>()
-            .ForCtorParam("FullAddress", opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
+            .ForMember(c => c.FullAddress, opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
+
+            // CreateMap<Company, CompanyDto>()
+            // .ForCtorParam("FullAddress", opt => opt.MapFrom(x => string.Join(' ', x.Address, x.Country)));
 
             CreateMap<Employee, EmployeeDto>();
+            CreateMap<CompanyForCreationDto, Company>();
+
         }
     }
 }
--- 02-Configuring a Logging Service in .NET Core/CompanyEmployees/Program.cs
+++ 02-Configuring a Logging Service in .NET Core/CompanyEmployees/Program.cs
@@ -13,10 +13,14 @@ builder.Services.ConfigureLoggerService();
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
 builder.Services.ConfigureSqlContext(builder.Configuration);
+
 builder.Services.AddControllers(config => {
     config.RespectBrowserAcceptHeader = true;
+    config.ReturnHttpNotAcceptable = true;
 }).AddXmlDataContractSerializerFormatters()
-.AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
+    .AddCustomCSVFormatter()
+    .AddApplicationPart(typeof(CompanyEmployees.Presentation.AssemblyReference).Assembly);
+
 builder.Services.AddAutoMapper(typeof(Program));
 
 
